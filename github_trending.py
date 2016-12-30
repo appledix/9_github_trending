a3a3e365ae9f3cceb9e18896385c33d6a2ff2bc7
appledix <dxl2010@mail.ru>
2016-12-31 02:57:45 +0300

    Метод кодирования гет-параметров запроса теперь осуществляется средствами модуля requests. Обновлено форматирование строк.

--- github_trending.py
+++ github_trending.py
@@ -8,17 +8,20 @@ def get_starting_date(time_period):
     return (datetime.today() - timedelta(days=time_period)).strftime("%Y-%m-%d")
 
 def get_trending_repositories(top_size, time_period):
+    payload = {'q':'created:>{}'.format(get_starting_date(time_period)),
+                'sort':'stars',
+                'per_page':top_size}
     request = \
-    requests.get('https://api.github.com/search/repositories?q=created:>%s&sort=stars&per_page=%d'
-        % (get_starting_date(time_period), top_size))
+    requests.get('https://api.github.com/search/repositories', params=payload)
     return request.json()["items"]
 
 if __name__ == '__main__':
-    print("%d best repositories on https://github.com for last %d days:"
-        % (SIZE_OF_THE_REPOSITORY_TOP, TIME_PERIOD))
+    print("{} best repositories on https://github.com for last {} days:".format(
+        SIZE_OF_THE_REPOSITORY_TOP,
+        TIME_PERIOD))
 
     for repository in get_trending_repositories(SIZE_OF_THE_REPOSITORY_TOP, TIME_PERIOD):
-        print("Repository name: %s\nLink: %s\nOpen issues amount: %s\n" 
-            % (repository["name"], 
-                repository["html_url"], 
-                repository["open_issues"]))
+        print("Repository name: {}\nLink: {}\nOpen issues amount: {}\n".format(
+            repository["name"],
+            repository["html_url"],
+            repository["open_issues"]))
