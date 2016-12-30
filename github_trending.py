43acb72f8dd075c34493498a788176de85349500
appledix <dxl2010@mail.ru>
2016-12-31 02:26:17 +0300

    Изменено имя переменной, хранящей размер топа репозиториев, убраны лишние комментарии.

--- github_trending.py
+++ github_trending.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime, timedelta
 
-TOP_SIZE = 20 # A number of the best repositories
-TIME_PERIOD = 7 # Days
+SIZE_OF_THE_REPOSITORY_TOP = 20
+TIME_PERIOD = 7
 
 def get_starting_date(time_period):
     return (datetime.today() - timedelta(days=time_period)).strftime("%Y-%m-%d")
@@ -15,9 +15,9 @@ def get_trending_repositories(top_size, time_period):
 
 if __name__ == '__main__':
     print("%d best repositories on https://github.com for last %d days:"
-        % (TOP_SIZE, TIME_PERIOD))
+        % (SIZE_OF_THE_REPOSITORY_TOP, TIME_PERIOD))
 
-    for repository in get_trending_repositories(TOP_SIZE, TIME_PERIOD):
+    for repository in get_trending_repositories(SIZE_OF_THE_REPOSITORY_TOP, TIME_PERIOD):
         print("Repository name: %s\nLink: %s\nOpen issues amount: %s\n" 
             % (repository["name"], 
                 repository["html_url"], 
